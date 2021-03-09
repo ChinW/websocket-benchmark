64a32d2634e11273f5f30b1350530a8458a39023
Chi <contractswif@hotmail.com>
2021-03-09 11:10:38 +0800

    add prometheus client for test

--- ws/server.js
+++ ws/server.js
@@ -1,5 +1,21 @@
+const http = require("http");
+const url = require("url");
 const WebSocket = require("ws");
 const { payload } = require("../utils.js");
+const promClient = require("prom-client");
+
+const register = new promClient.Registry();
+register.setDefaultLabels({
+  app: "myApp",
+});
+promClient.collectDefaultMetrics({ register });
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_seconds_custom',
+  help: 'Duration of HTTP requests in microseconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+})
+register.registerMetric(httpRequestDurationMicroseconds)
 
 const MAX_CLIENTS = 5000;
 const SHARES = {
@@ -12,8 +28,6 @@ const SHARES = {
 const SHARE_NAMES = ["NFLX", "TSLA", "AMZN", "GOOG", "NVDA"];
 let transactionsPerSecond = 0;
 let connectedClients = 0;
-
-const wss = new WebSocket.Server({ port: 8080, perMessageDeflate: false });
 
 const sendMessageTest = (ws, ms) => {
   const share = SHARE_NAMES[parseInt(Math.random() * SHARES.length)];
@@ -29,8 +43,36 @@ const sendMessageTest = (ws, ms) => {
   }, ms);
 };
 
+const authenticate = (request, cb) => {
+  console.log("request.headers", request.headers);
+  cb(false, true);
+};
 
-wss.on("connection", (ws) => {
+const server = http.createServer(async (req, res) => {
+  const route = url.parse(req.url).pathname;
+  const end = httpRequestDurationMicroseconds.startTimer()
+  if (route === "/metrics") {
+    res.setHeader("Content-Type", register.contentType);
+    res.end(await register.metrics());
+  }
+  end({ route, code: res.statusCode, method: req.method })
+});
+const wss = new WebSocket.Server({ noServer: true, perMessageDeflate: false });
+
+server.on("upgrade", (request, socket, head) => {
+  authenticate(request, (err, client) => {
+    if (err || !client) {
+      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+      socket.destroy();
+      return;
+    }
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit("connection", ws, 1, 2, 3, 4, 5);
+    });
+  });
+});
+
+wss.on("connection", (ws, ...args) => {
   connectedClients++;
   sendMessageTest(ws, 50);
   ws.on("message", (message) => {
@@ -61,3 +103,5 @@ setInterval(() => {
   transactionsPerSecond = 0;
   last = Date.now();
 }, 2000);
+
+server.listen(8080);
